28dbf00c6b26ded04bb4fb41af83db1d12509677
Jerry Huang <66283343+jerrhuang@users.noreply.github.com>
2026-01-05 16:38:36 -0800

    events.py for bubble sort and docstrings

--- src/engine/events.py
+++ src/engine/events.py
@@ -17,19 +17,53 @@ Events do not contain rendering, timing, or UI logic.
 from dataclasses import dataclass
 
 class Event:
+    """
+    Base class for all execution trace events.
+
+    An Event represents a single, atomic fact about an algorithm's execution.
+    Subclasses define the specific type of action (e.g., comparison, swap).
+
+    Events contain no behavior and are interpreted by the Player.
+    """
     pass
 
 @dataclass(frozen=True)
 class Compare(Event):
+    """
+    Indicates that two elements were compared.
+
+    Attributes:
+        i (int): Index of the first element.
+        j (int): Index of the second element.
+    """
     i: int
     j: int
 
 @dataclass(frozen=True)
 class Swap(Event):
+    """
+    Indicates that two elements were swapped.
+
+    Attributes:
+        i (int): Index of the first element.
+        j (int): Index of the second element.
+    """
     i: int
     j: int
 
 @dataclass(frozen=True)
 class SetPointer(Event):
+    """
+    Sets or moves a named pointer to a specific index.
+
+    These "pointers" are defined only by the algorithm and is
+    interpreted by the Player and Renderer.
+
+    Attributes:
+        name (str): Name of the pointer.
+        location (int): Target location:
+                            - index of arrays
+                            - node id for graphs or trees
+    """
     name: int
-    index: int
+    location: int
